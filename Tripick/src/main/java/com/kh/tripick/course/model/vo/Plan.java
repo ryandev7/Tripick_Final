edf1531a464a3f3f961d2fedd37380a3d3e13f97
arrram <aram4711@gmail.com>
2022-08-12 17:39:44 +0900

    김아람 vo
    plan, planner vo

--- Tripick/src/main/java/com/kh/tripick/course/model/vo/Plan.java
+++ Tripick/src/main/java/com/kh/tripick/course/model/vo/Plan.java
@@ -1,5 +1,25 @@
 package com.kh.tripick.course.model.vo;
 
-public class Plan {
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Getter @Setter @ToString
+@AllArgsConstructor
+public class Plan {	
+	private int planNo; // SEQ_PLNO
+	private int refPno;
+	private int planOrder;
+	private String placeName;
+	private String placeAddress;
+	private String xCoordinate;
+	private String yCoordinate;
+	private String tripDate;
+	private String memo;
 
 }
--- Tripick/src/main/java/com/kh/tripick/course/model/vo/Planner.java
+++ Tripick/src/main/java/com/kh/tripick/course/model/vo/Planner.java
@@ -1,5 +1,27 @@
 package com.kh.tripick.course.model.vo;
 
-public class Planner {
+import java.sql.Date;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@NoArgsConstructor
+@Getter @Setter @ToString
+@AllArgsConstructor
+public class Planner {
+	private int plannerNo; // SEQ_PNO
+	private String plannerWriter;
+	private String plannerTitle;
+	private Date fDate;
+	private Date lDate;
+	private int wDate;
+	private String area;
+	private String type;
+	private int count;
+	private Date createDate;
+	private String originName;
+	private String changeName;
+	private String status;
 }
